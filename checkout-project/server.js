d9574cbbb8aabd1a222423c7b78fc8af3128b5f5
Hearth and Heal <hello@hearthandheal.org>
2026-04-22 00:27:41 +0300

    Add simple HTML checkout project as additional option

---
+++ checkout-project/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Checkout server running on port ${PORT}`);
+});
